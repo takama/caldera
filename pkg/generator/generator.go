86b77044357560156dcf028a18dad8ef273255ae
Igor Dolzhikov <bluesriverz@gmail.com>
2018-09-15 19:04:58 +0700

    Implemented generator common method

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,9 +1,38 @@
 package generator
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+
 	"github.com/takama/caldera/pkg/config"
+	"github.com/takama/caldera/pkg/helper"
 )
 
 // Run generator
 func Run(cfg *config.Config) {
+	helper.LogF("Copy base templates", copyTemplates(
+		path.Join(cfg.Directories.Templates, config.Base),
+		cfg.Directories.Service,
+	))
+	helper.LogF("Render templates", render(cfg))
+	helper.LogF("Could not change directory", os.Chdir(cfg.Directories.Service))
+	log.Println("Vendors initialization")
+
+	log.Println("Git repository initialization")
+	helper.LogF("Could not init git", Exec("git", "init"))
+	helper.LogF("Could not add git files", Exec("git", "add", "--all"))
+	helper.LogF("Could not commit git files", Exec("git", "commit", "-m", "'Initial commit'"))
+	fmt.Printf("New repository was created, use command 'cd %s'", cfg.Directories.Service)
+}
+
+// Exec runs the commands
+func Exec(command ...string) error {
+	execCmd := exec.Command(command[0], command[1:]...) // nolint: gosec
+	execCmd.Stderr = os.Stderr
+	execCmd.Stdout = os.Stdout
+	execCmd.Stdin = os.Stdin
+	return execCmd.Run()
 }
