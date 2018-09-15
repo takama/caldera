2ffc18de9c7790e10ff7082a8584773ea990cdca
Igor Dolzhikov <bluesriverz@gmail.com>
2018-09-16 03:07:47 +0700

    Added version package

---
+++ .templates/base/pkg/version/version.go
@@ -0,0 +1,14 @@
+package version
+
+var (
+	// RELEASE returns the release version
+	RELEASE = "UNKNOWN"
+	// DATE returns the release date
+	DATE = "UNKNOWN"
+	// REPO returns the git repository URL
+	REPO = "UNKNOWN"
+	// COMMIT returns the short sha from git
+	COMMIT = "UNKNOWN"
+	// BRANCH returns deployed git brunch
+	BRANCH = "UNKNOWN"
+)
