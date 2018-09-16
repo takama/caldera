2acb8d433441df8bf4bc0938d7dd564470751c72
Igor Dolzhikov <bluesriverz@gmail.com>
2018-09-17 05:45:56 +0700

    Added info package

---
+++ .templates/base/pkg/info/info.go
@@ -0,0 +1,168 @@
+package info
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/{{[ .Github ]}}/{{[ .Name ]}}/pkg/version"
+
+	"go.uber.org/zap"
+)
+
+// Config contains params to setup info handler
+type Config struct {
+	Port       int
+	Statistics bool
+}
+
+// ProbeChecker defines simple function for probe checks
+type ProbeChecker func() error
+
+// Service contains info/health-check functionality
+type Service struct {
+	handlers        map[string]http.HandlerFunc
+	logger          *zap.Logger
+	livenessProbes  []ProbeChecker
+	readinessProbes []ProbeChecker
+}
+
+// NewService creates new service with info/health-check handlers
+func NewService(logger *zap.Logger) *Service {
+	return &Service{
+		logger:   logger,
+		handlers: make(map[string]http.HandlerFunc),
+	}
+}
+
+// AddHandler adds new handler with given path to info service
+func (s *Service) AddHandler(path string, handler http.HandlerFunc) {
+	s.handlers[path] = handler
+}
+
+// RegisterLivenessProbe defines liveness probe function
+func (s *Service) RegisterLivenessProbe(checker ProbeChecker) {
+	s.livenessProbes = append(s.livenessProbes, checker)
+}
+
+// RegisterReadinessProbe defines readiness probe function
+func (s *Service) RegisterReadinessProbe(checker ProbeChecker) {
+	s.readinessProbes = append(s.livenessProbes, checker)
+}
+
+// Run info/health-check service
+func (s *Service) Run(addr string) *http.Server {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: s,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			// Check for known errors
+			if err != context.DeadlineExceeded &&
+				err != context.Canceled &&
+				err != http.ErrServerClosed {
+				s.logger.Fatal(err.Error())
+			}
+			s.logger.Warn(err.Error())
+		}
+	}()
+
+	return srv
+}
+
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.logger.Debug(r.Method,
+		zap.String("addr", r.RemoteAddr),
+		zap.String("url", r.URL.Path),
+	)
+	route := strings.TrimRight(r.URL.Path, " /")
+	switch r.Method {
+	case "GET":
+		switch route {
+		case "", "/info":
+			s.info(w)
+		case "/healthz":
+			s.liveness(w)
+		case "/readyz":
+			s.readiness(w)
+		default:
+			for path, handler := range s.handlers {
+				if route == path {
+					handler(w, r)
+					return
+				}
+			}
+			http.NotFound(w, r)
+		}
+	default:
+		if r.Method == "OPTIONS" {
+			switch route {
+			case "", "/info", "/healthz", "/readyz":
+				w.Header().Set("Allow", "GET")
+			default:
+				for path := range s.handlers {
+					if route == path {
+						w.Header().Set("Allow", "GET")
+						return
+					}
+				}
+				http.NotFound(w, r)
+			}
+			return
+		}
+		http.Error(w, "Only GET is allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (s *Service) info(w http.ResponseWriter) {
+	data, err := json.Marshal(
+		map[string]string{
+			"version": version.RELEASE + "-" + version.COMMIT + "-" + version.BRANCH,
+			"date":    version.DATE,
+			"repo":    version.REPO,
+		},
+	)
+
+	if err != nil {
+		s.writeError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(data); err != nil {
+		s.logger.Error(err.Error())
+	}
+}
+
+func (s *Service) liveness(w http.ResponseWriter) {
+	for _, checker := range s.livenessProbes {
+		if err := checker(); err != nil {
+			s.writeError(w, err)
+			return
+		}
+	}
+	if _, err := io.WriteString(w, "Ok"); err != nil {
+		s.logger.Error(err.Error())
+	}
+}
+
+func (s *Service) readiness(w http.ResponseWriter) {
+	for _, checker := range s.readinessProbes {
+		if err := checker(); err != nil {
+			s.writeError(w, err)
+			return
+		}
+	}
+	if _, err := io.WriteString(w, "Ok"); err != nil {
+		s.logger.Error(err.Error())
+	}
+}
+
+func (s *Service) writeError(w http.ResponseWriter, err error) {
+	message := err.Error()
+	s.logger.Error(message)
+	http.Error(w, message, http.StatusInternalServerError)
+}
---
+++ .templates/base/pkg/info/info_test.go
@@ -0,0 +1,134 @@
+package info
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/{{[ .Github ]}}/{{[ .Name ]}}/pkg/logger"
+	"github.com/{{[ .Github ]}}/{{[ .Name ]}}/pkg/version"
+)
+
+var ErrReturnError = errors.New("Test of return Error")
+
+func testHandler(
+	t *testing.T, handler http.HandlerFunc, method, path string, code int, body string) {
+	req, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	trw := httptest.NewRecorder()
+	handler.ServeHTTP(trw, req)
+
+	if trw.Code != code {
+		t.Error("Expected status code:", code, "got", trw.Code)
+	}
+	if trw.Body.String() != body {
+		t.Error("Expected body", body, "got", trw.Body.String())
+	}
+}
+
+func TestProbe(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	testHandler(
+		t, service.ServeHTTP, "GET", "/healthz",
+		http.StatusOK, "Ok",
+	)
+	testHandler(
+		t, service.ServeHTTP, "GET", "/readyz",
+		http.StatusOK, "Ok",
+	)
+	service.RegisterLivenessProbe(func() error {
+		return ErrReturnError
+	})
+	service.RegisterReadinessProbe(func() error {
+		return ErrReturnError
+	})
+	testHandler(
+		t, service.ServeHTTP, "GET", "/healthz",
+		http.StatusInternalServerError, ErrReturnError.Error()+"\n",
+	)
+	testHandler(
+		t, service.ServeHTTP, "GET", "/readyz",
+		http.StatusInternalServerError, ErrReturnError.Error()+"\n",
+	)
+}
+
+func TestNotAllowed(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	testHandler(
+		t, service.ServeHTTP, "POST", "/",
+		http.StatusMethodNotAllowed, "Only GET is allowed\n",
+	)
+}
+
+func TestOptions(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	testHandler(
+		t, service.ServeHTTP, "OPTIONS", "/",
+		http.StatusOK, "",
+	)
+}
+
+func TestNotFound(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	testHandler(
+		t, service.ServeHTTP, "GET", "/notfound",
+		http.StatusNotFound, "404 page not found\n",
+	)
+	testHandler(
+		t, service.ServeHTTP, "OPTIONS", "/notfound",
+		http.StatusNotFound, "404 page not found\n",
+	)
+}
+
+func TestAddHandler(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	service.AddHandler(
+		"/handler", func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte("Handler"))
+			if err != nil {
+				t.Error(err)
+			}
+		},
+	)
+	testHandler(
+		t, service.ServeHTTP, "GET", "/handler",
+		http.StatusOK, "Handler",
+	)
+	testHandler(
+		t, service.ServeHTTP, "OPTIONS", "/handler",
+		http.StatusOK, "",
+	)
+}
+
+func TestInfo(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	data, err := json.Marshal(
+		map[string]string{
+			"version": version.RELEASE + "-" + version.COMMIT + "-" + version.BRANCH,
+			"date":    version.DATE,
+			"repo":    version.REPO,
+		},
+	)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	testHandler(
+		t, service.ServeHTTP, "GET", "/info",
+		http.StatusOK, string(data),
+	)
+}
+
+func TestRun(t *testing.T) {
+	service := NewService(logger.New(new(logger.Config)))
+	h := service.Run(":0")
+	if h == nil {
+		t.Error("Expected HTTP handler, got nil")
+	}
+}
