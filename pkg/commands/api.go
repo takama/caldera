8053bbe3cee71589a4a5a6f55d6d6895ff5cb83d
Igor Dolzhikov <bluesriverz@gmail.com>
2018-12-12 16:03:57 +0100

    Fixed linter issues for init and globals

--- pkg/commands/api.go
+++ pkg/commands/api.go
@@ -1,4 +1,6 @@
 // Package commands process flags/environment variables/config file
+// It contains global variables with configs and commands
+// nolint: gochecknoglobals, gochecknoinits
 package commands
 
 import (
