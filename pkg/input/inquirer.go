7d729a823c2183ca7d6a130d67e4f0590ccde1d5
Igor Dolzhikov <bluesriverz@gmail.com>
2019-10-18 13:22:00 +0200

    Fixed linter issues

--- pkg/input/inquirer.go
+++ pkg/input/inquirer.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Inquire for configuration
-// nolint: funlen
+// nolint: funlen, gocognit
 func Inquire(cfg *config.Config) *config.Config {
 	cfg.Github = StringAnswer("Provide name for your Github account", cfg.Github)
 	cfg.Name = StringAnswer("Provide name for your service", cfg.Name)
@@ -18,14 +18,19 @@ func Inquire(cfg *config.Config) *config.Config {
 	cfg.Project = StringAnswer("Provide project name", path.Join("github.com", cfg.Github, cfg.Name))
 	cfg.Bin = StringAnswer("Provide binary file name", cfg.Name)
 	apis := []string{config.APIGateway, config.APIgRPC}
+
 	var count int
+
 	question := "Do you need API for the service? "
+
 	for len(apis) > 0 {
 		if count > 0 {
 			question = "Do you need one more API for the service?"
 		}
+
 		if BoolAnswer(question) {
 			cfg.API.Enabled = true
+
 			switch OptionAnswer("What kind of API do you need?", apis...) {
 			case config.APIGateway:
 				apis = delete(apis, config.APIGateway)
@@ -44,6 +49,7 @@ func Inquire(cfg *config.Config) *config.Config {
 		}
 		count++
 	}
+
 	if cfg.API.Enabled {
 		if BoolAnswer("Do you want to terminate API with TLS") {
 			cfg.API.Config.Insecure = false
@@ -59,10 +65,13 @@ func Inquire(cfg *config.Config) *config.Config {
 			cfg.API.Config.Insecure = true
 		}
 	}
+
 	storages := []string{config.StoragePostgres, config.StorageMySQL}
 	question = "Do you need storage driver?"
+
 	if BoolAnswer(question) {
 		cfg.Storage.Enabled = true
+
 		switch OptionAnswer("What kind of storage driver do you need?", storages...) {
 		case config.StoragePostgres:
 			cfg.Storage.Postgres = true
@@ -74,28 +83,35 @@ func Inquire(cfg *config.Config) *config.Config {
 	} else {
 		cfg.Storage.Enabled = false
 	}
+
 	if cfg.API.Enabled && cfg.Storage.Enabled &&
 		BoolAnswer("Do you need Contract API example for the service?") {
 		cfg.Example = true
 	}
+
 	if BoolAnswer("Do you want to deploy your service to the Google Kubernetes Engine?") {
 		cfg.GKE.Enabled = true
 		cfg.GKE.Project = StringAnswer("Provide ID of your project on the GCP", cfg.GKE.Project)
 		cfg.GKE.Zone = StringAnswer("Provide compute zone of your project on the GCP", cfg.GKE.Zone)
 		cfg.GKE.Cluster = StringAnswer("Provide cluster name in the GKE", cfg.GKE.Cluster)
 	}
+
 	if !path.IsAbs(cfg.Directories.Templates) {
 		if currentDir, err := os.Getwd(); err == nil {
 			cfg.Directories.Templates = path.Join(currentDir, cfg.Directories.Templates)
 		}
 	}
+
 	cfg.Directories.Templates = StringAnswer("Templates directory", cfg.Directories.Templates)
+
 	if cfg.Directories.Service == "" {
 		if goPath := os.Getenv("GOPATH"); goPath != "" {
 			cfg.Directories.Service = path.Join(goPath, "src", cfg.Project)
 		}
 	}
+
 	cfg.Directories.Service = StringAnswer("New service directory", cfg.Directories.Service)
+
 	if BoolAnswer("Do you want initialize service repository with git") {
 		cfg.GitInit = true
 	} else {
@@ -112,5 +128,6 @@ func delete(src []string, value string) (dst []string) {
 			dst = append(src[:i], src[i+1:]...)
 		}
 	}
+
 	return
 }
