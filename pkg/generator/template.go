a817d574da74a8189ca3e7be5f366e44ed3cf3de
Igor Dolzhikov <bluesriverz@gmail.com>
2021-04-17 23:26:48 +0200

    Changed approach for random string generation

--- pkg/generator/template.go
+++ pkg/generator/template.go
@@ -1,8 +1,8 @@
 package generator
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,13 +13,16 @@ import (
 	"github.com/takama/caldera/pkg/helper"
 )
 
+const randomStringLength = 32
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 // render executes templates in service directory with configured data.
 func render(cfg *config.Config) error {
 	return filepath.Walk(
 		cfg.Directories.Service,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				return fmt.Errorf("failed to scan service directory: %s", err)
+				return fmt.Errorf("failed to scan service directory: %w", err)
 			}
 			if info.IsDir() {
 				return nil
@@ -30,37 +33,52 @@ func render(cfg *config.Config) error {
 				Funcs(
 					template.FuncMap{
 						"toENV": func(str string) string {
-							return strings.ToUpper(strings.Replace(str, "-", "_", -1))
+							return strings.ToUpper(strings.ReplaceAll(str, "-", "_"))
 						},
 						"currentYear": func() int {
 							return time.Now().Year()
 						},
 						"randStr": func() string {
-							var ch = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-							b := make([]rune, 32)
-							for i := range b {
-								b[i] = ch[rand.Intn(len(ch))]
+							str, err := randomString(randomStringLength, chars)
+							if err != nil {
+								panic(err)
 							}
-							return string(b)
+
+							return str
 						},
 					},
 				).ParseFiles(path)
 			if err != nil {
-				return fmt.Errorf("could not parse template: %s", err)
+				return fmt.Errorf("could not parse template: %w", err)
 			}
 			f, err := os.Create(path)
 			if err != nil {
-				return fmt.Errorf("could not create file: %s", err)
+				return fmt.Errorf("could not create file: %w", err)
 			}
 			defer func() {
 				helper.LogE("could not close file", f.Close())
 			}()
 
 			if err := tpl.Execute(f, cfg); err != nil {
-				return fmt.Errorf("could not execute template: %s", err)
+				return fmt.Errorf("could not execute template: %w", err)
 			}
 
 			return nil
 		},
 	)
+}
+
+// randomString generates a random string with the requested length from the source of chars.
+func randomString(length int, source string) (string, error) {
+	bytes := make([]byte, length)
+
+	if _, err := rand.Read(bytes); err != nil {
+		return "", fmt.Errorf("could not generate random string: %w", err)
+	}
+
+	for i, b := range bytes {
+		bytes[i] = source[b%byte(len(source))]
+	}
+
+	return string(bytes), nil
 }
