36b63e4ac4c6f04f71836ebf95929b3afb75f57d
Igor Dolzhikov <bluesriverz@gmail.com>
2021-04-17 18:51:56 +0200

    Used errors.Is

--- .templates/base/pkg/system/operations_test.go
+++ .templates/base/pkg/system/operations_test.go
@@ -1,6 +1,7 @@
 package system_test
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
@@ -11,13 +12,13 @@ func TestStubHandling(t *testing.T) {
 	operator := system.NewOperator(&http.Server{})
 	err := operator.Reload()
 
-	if err != system.ErrNotImplemented {
+	if !errors.Is(err, system.ErrNotImplemented) {
 		t.Error("Expected error", system.ErrNotImplemented, "got", err)
 	}
 
 	err = operator.Maintenance()
 
-	if err != system.ErrNotImplemented {
+	if !errors.Is(err, system.ErrNotImplemented) {
 		t.Error("Expected error", system.ErrNotImplemented, "got", err)
 	}
 
